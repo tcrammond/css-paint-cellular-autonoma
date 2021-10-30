b1333304d1b0de6230d8736878493ca823b30640
Tyler Crammond <tylerchc92@gmail.com>
2021-10-30 11:23:23 +0100

    Load polyfill before rendering

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import 'css-paint-polyfill'
-
 import App from './App.svelte'
-
 import workletURL from "./worklet.js?url"
 
-(async function () {
+// Make sure the polyfill is loaded before rendering the app.
+if (window.CSS && window.CSS.paintWorklet) {
+  init()
+} else {
+  import('css-paint-polyfill').then(init)
+}
+
+async function init () {
   CSS.paintWorklet.addModule(workletURL)
-})()
-
-const app = new App({
-  target: document.getElementById('app')
-})
-
-export default app
+  const app = new App({
+    target: document.getElementById('app')
+  })
+}
